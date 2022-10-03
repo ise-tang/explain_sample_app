425a124182628d321653fe158b62d682ca4cb1d3
nakamura.issei <nakamura.issei@moneyforward.co.jp>
2022-10-03 09:15:16 +0900

    category seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+%w{文学 人文 社会 ノンフィクション 歴史 ビジネス 投資 科学 医学 コンピュータ 趣味 スポーツ 資格 暮らし 旅行ガイド 語学 英語学習 教育 絵本 コミック ライトノベル ゲーム攻略本 新書}.each.with_index(1) do |cate, idx|
+  category = Category.find_by(id: idx) || Category.new(name: cate)
+  category.save!
+end
